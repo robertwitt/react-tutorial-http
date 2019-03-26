b01a95379ab6bd243f545d2528750012eb7a164e
Robert Witt <robert.witt@zalando.de>
2019-03-26 07:01:57 +0100

    Transform post data on Blog component.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,7 +15,14 @@ class Blog extends Component {
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        this.setState({ posts: response.data });
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Max'
+          }
+        });
+        this.setState({ posts: updatedPosts });
         // console.log(response);
       });
   }
@@ -24,7 +31,8 @@ class Blog extends Component {
     const posts = this.state.posts.map(post => (
       <Post
         key={post.id}
-        title={post.title}/>
+        title={post.title}
+        author={post.author}/>
     ));
 
     return (
