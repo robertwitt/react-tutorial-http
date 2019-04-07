a9a3d4142226fa9875908b817dc59bc322428c9b
Robert Witt <robert.witt@zalando.de>
2019-04-07 16:12:08 +0200

    Use nested route in Posts component to display full post.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -15,13 +14,13 @@ class Blog extends Component {
             <ul>
               <li>
                 <NavLink
-                  to="/"
+                  to="/posts/"
                   exact
                   activeClassName="my-active"
                   activeStyle={{
                     color: '#fa923f',
                     textDecoration: 'underline'
-                  }}>Home</NavLink>
+                  }}>Posts</NavLink>
               </li>
               <li><NavLink to={{
                 pathname: '/new-post',
@@ -32,9 +31,8 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/" exact component={Posts} />
           <Route path="/new-post" component={NewPost} />
-          <Route path="/:id" exact component={FullPost} />
+          <Route path="/posts" component={Posts} />
         </Switch>
       </div>
     );
